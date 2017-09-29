cf8ef22561076256de18151a7ab6f0ba24302f7c
Keishi4 <31190897+Keishi4@users.noreply.github.com>
2017-09-29 14:00:53 +0300

    mk_cmd
    added release/debug echo descr.

--- mk_chess.cmd
+++ mk_chess.cmd
@@ -1,8 +1,8 @@
 @SET CC="D:\Portable\tcc-win32\tcc.exe"
 @SET CC64="D:\Portable\tcc-win64\tcc.exe"
-@ECHO *** making 32-bit executable ***
+@ECHO *** making debug 32-bit executable ***
 @%CC% -D_DTEST -Wall -o chess32d.exe chess.c >>compile.log
-@ECHO *** making 64-bit executable ***
+@ECHO *** making debug 64-bit executable ***
 @%CC64% -D_DTEST -Wall -o chess64d.exe chess.c >>compl64.log
 @SET CC=
 @SET CC64=
--- mk_chess_rel.cmd
+++ mk_chess_rel.cmd
@@ -1,8 +1,8 @@
 @SET CC="D:\Portable\tcc-win32\tcc.exe"
 @SET CC64="D:\Portable\tcc-win64\tcc.exe"
-@ECHO *** making 32-bit executable ***
+@ECHO *** making release 32-bit executable ***
 @%CC% -Wall -o chess32.exe chess.c >>compile.log
-@ECHO *** making 64-bit executable ***
+@ECHO *** making release 64-bit executable ***
 @%CC64% -Wall -o chess64.exe chess.c >>compl64.log
 DIR *.log *.exe
 @SET CC=
